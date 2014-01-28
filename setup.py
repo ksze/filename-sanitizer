c7eff9b9dec66b13fecb0d22b264309b4de5d610
Kal Sze <kal.sze@alvanon.com>
2014-01-28 09:43:23 +0800

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'sanitize',
+    version = '0.0.1',
+    author = 'Kal Sze (twitter.com/k_sze, github.com/ksze)',
+    author_email = 'swordangel@gmail.com',
+    description = 'A Python 2.7/3.3 module to sanitize filenames in a (hopefully) cross-platform, cross-filesystem manner.',
+    license = 'BSD',
+    keywords = 'filename sanitization',
+    url = 'https://github.com/ksze/sanitize',
+    packages = ['sanitize'],
+    long_description = read('README.md'),
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
