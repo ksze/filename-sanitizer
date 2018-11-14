e990e963dc5bd1d172dbc8f85ebd383f6a984ec7
Kal Sze <swordangel@gmail.com>
2018-11-15 00:45:22 +0800

    Prepare for v0.1.0 beta release.

--- setup.py
+++ setup.py
@@ -13,19 +13,27 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = 'sanitize',
-    version = '0.0.2',
+    name = 'filename-sanitizer',
+    version = '0.1.0',
     author = 'Kal Sze (twitter.com/k_sze, github.com/ksze)',
     author_email = 'swordangel@gmail.com',
-    description = 'A Python 2.7/3.2+ module to sanitize filenames in a (hopefully) cross-platform, cross-filesystem manner.',
+    description = 'A Python 2.7/3.2+ package to sanitize filenames in a (hopefully) cross-platform, cross-filesystem manner.',
     license = 'BSD',
     keywords = 'filename sanitization',
-    url = 'https://github.com/ksze/sanitize',
-    py_modules = ['sanitize'],
+    url = 'https://github.com/ksze/filename-sanitizer',
+    packages = ['filename_sanitizer'],
     long_description = read('README.md'),
     classifiers = [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Topic :: Utilities',
         'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: System :: Filesystems',
     ],
 )
